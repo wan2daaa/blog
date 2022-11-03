a8fc6aa91e2564e2ed379e5602602ff5de512cc3
JaeWan2 <wan2daaa@gmail.com>
2022-11-03 23:16:37 +0900

    기본 프론트 세팅

--- src/main/java/wane/blog/controller/ViewController.java
+++ src/main/java/wane/blog/controller/ViewController.java
@@ -1,9 +1,33 @@
 package wane.blog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController {
 
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/study")
+    public String study() {
+        return "study";
+    }
+
+    @RequestMapping("/board")
+    public String board() {
+        return "board";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+    @RequestMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
 
 }
